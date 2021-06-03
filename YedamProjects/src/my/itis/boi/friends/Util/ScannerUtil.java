4d4b7a1877b46a91d4bc3f05d1f7e063388bd88d
TaeHun <TaeHun@DESKTOP-24SJO3O>
2021-06-03 22:32:10 +0900

    home

--- YedamProjects/src/my/itis/boi/friends/Util/ScannerUtil.java
+++ YedamProjects/src/my/itis/boi/friends/Util/ScannerUtil.java
@@ -8,16 +8,21 @@ public class ScannerUtil {
 	static Scanner scanner  = new Scanner(System.in);
 	
 	public static int readInt() { // 입력받은 값을 정수로 자동변환
-		
+		int i = scanner.nextInt();
+		return i;
 	}
 	public static String readStr() { // 입력받은 값을 문자열
-		
+		String s = scanner.next();
+		return s;
 	}
 	public static String readDate() { // 입력받은 값을 날짜로 (예외처리 필수)
-	
+		
 	}
 	public static Friends readFriend() { // 입력받은 값을 split하여 Friend객체를 생성해서 담는다?
-	
+		String fr = scanner.next();
+		String[] arr = fr.split(" ");
+		Friends friends = new Friends(arr[0], arr[1], arr[2]);
+		return friends;
 	}
 
 }
